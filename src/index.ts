973d97227e396e92cb6f90a6d38896d35817c3bb
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-11-04 02:22:07 -0300

    feat: add `cors config`
    add cors config for the front-end port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
 import routes from "./routes";
+import cors from "cors";
 
 AppDataSource.initialize().then(() => {
   const app = express();
+
+  const allowedOrigin = [`http://localhost:${process.env.FRONT_PORT}`];
+
+  const options: cors.CorsOptions = {
+    origin: allowedOrigin,
+  };
+
+  app.use(cors(options));
 
   app.use(express.json());
   app.use(routes);
